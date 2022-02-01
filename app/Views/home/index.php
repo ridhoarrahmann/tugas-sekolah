98c4661beee3844cb030e20b44b0a7b6419064a1
ridhoarrahmann <ridhoarrahman7@gmail.com>
2022-02-01 08:47:32 +0700

    update file

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -3,19 +3,27 @@
 <div class="about">
 	
 <div class="row d-flex ">
-	<div class="col-md-6 d-flex profile-pic-container ">
-			<div class="profile-pic">
-				<img src="<?php echo base_url('assets/images/hansohee.jpg') ?>">
+	<div class="col-lg-6 d-flex profile-pic-container ">
 
-			</div>
+		<img class="profile-pic" src="<?php echo base_url('assets/images/hansohee.jpg') ?>">
+				
+
+			
+			<!-- <div class="profile-pic">
+				<img src="">
+
+			</div> -->
 	</div>
-	<div class="col-md-6 d-flex basic-profile ">
+	<div class="col-lg-6 d-flex basic-profile ">
 		<div class="basic-profile">
 			<h1><?php echo $firstName; ?></h1>
 			<h1><span><?php echo $lastName; ?></span></h1>
 			<h3><?php echo $career; ?></h3>
-
+			<div class="button-about col-md-12">
+				<button type="button" class="btn btn-primary">About Me</button>
+			</div>
 	</div>
+
 	</div>
 </div>
 
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -13,9 +13,9 @@
 
 <!-- navbar -->
 
-
+<div class="navbar-container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+  <div class="container">
     <!-- <a class="navbar-brand" href="#">Navbar</a> -->
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -30,6 +30,7 @@
     </div>
   </div>
 </nav>
+</div>
 
 
 
